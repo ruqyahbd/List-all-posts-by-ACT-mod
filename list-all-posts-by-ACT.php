3d85ca1c0226c1b6b995f4b28bbe872f3504d52a
Fabio Marzocca <marzoccafabio@gmail.com>
2014-12-28 11:27:18 +0100

    1st

---
+++ list-all-posts-by-ACT.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: List all posts by Author, nested Categories and Titles
+Plugin URI: http://www.marzoca.net/
+Description: A plugin to list all posts by Author, nested Categories and Titles
+Version: 1.0
+Author: Fabio Marzocca
+Author URI: http://www.marzocca.net
+License: GPL2
+*/
+?>
